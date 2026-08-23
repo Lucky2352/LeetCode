75dac6799dc1dd854ba27ee5758f5642bfd353b1
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-23 23:02:45 +0530

    LC-flood-fill [Runtime: N/A; Memory: N/A]

--- depth-first-search/flood-fill.java
+++ depth-first-search/flood-fill.java
@@ -10,36 +10,30 @@ class Solution {
     public int[][] floodFill(int[][] grid, int sr, int sc, int color) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] check = new int[n][m];
         Queue<pair> q = new LinkedList<>();
         int oldColor = grid[sr][sc];
         if (oldColor == color) {
             return grid;
         }
-        check[sr][sc] = 1;
         grid[sr][sc] = color;
         q.offer(new pair(sr, sc));
         while (!q.isEmpty()) {
             pair temp = q.poll();
             int row = temp.row;
             int col = temp.col;
-            if (row - 1 >= 0 && grid[row - 1][col] == oldColor && check[row - 1][col] == 0) {
-                check[row - 1][col] = 1;
+            if (row - 1 >= 0 && grid[row - 1][col] == oldColor) {
                 grid[row - 1][col] = color;
                 q.offer(new pair(row - 1, col));
             }
-            if (row + 1 < n && grid[row + 1][col] == oldColor && check[row + 1][col] == 0) {
-                check[row + 1][col] = 1;
+            if (row + 1 < n && grid[row + 1][col] == oldColor) {
                 grid[row + 1][col] = color;
                 q.offer(new pair(row + 1, col));
             }
-            if (col - 1 >= 0 && grid[row][col - 1] == oldColor && check[row][col - 1] == 0) {
-                check[row][col - 1] = 1;
+            if (col - 1 >= 0 && grid[row][col - 1] == oldColor) {
                 grid[row][col - 1] = color;
                 q.offer(new pair(row, col - 1));
             }
-            if (col + 1 < m && grid[row][col + 1] == oldColor && check[row][col + 1] == 0) {
-                check[row][col + 1] = 1;
+            if (col + 1 < m && grid[row][col + 1] == oldColor) {
                 grid[row][col + 1] = color;
                 q.offer(new pair(row, col + 1));
             }
