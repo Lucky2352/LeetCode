a0dda20a1d006e693362544212c4d866496e7ed2
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-24 22:19:56 +0530

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

--- bit-manipulation/divide-two-integers.java
+++ bit-manipulation/divide-two-integers.java
@@ -2,6 +2,20 @@ class Solution {
     public int divide(int dividend, int divisor) {
         if (dividend == Integer.MIN_VALUE && divisor == -1)
             return Integer.MAX_VALUE;
-        return dividend / divisor;
+        int sign = 1;
+        if(dividend < 0){
+            dividend *= -1;
+            sign *= -1;
+        }
+        if(divisor < 0){
+            divisor *= -1;
+            sign  *= -1;
+        }
+            int count = 0;
+        while(dividend >= divisor){
+            count++;
+            dividend -= divisor;
+        }
+        return sign * count;
     }
 }
