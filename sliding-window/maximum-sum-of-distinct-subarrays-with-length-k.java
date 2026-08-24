dbd49ff7d7ea7e050641fcce045a316312a13cf3
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-24 11:41:13 +0530

    LC-maximum-sum-of-distinct-subarrays-with-length-k [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ sliding-window/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,23 +1,33 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int i = 0;
+        int j = 0;
         long sum = 0;
-        for(int i = 0;i<=nums.length - k;i++){
-            Set<Integer> set = new HashSet<>();
-            long cur = 0;
-            long count = 0;
-            for(int j = i;j<i+k;j++){
-                if(!set.contains(nums[j])){
-                    cur += nums[j];
-                    set.add(nums[j]);
-                    count++;
-                }else{
-                    break;
+        long maxi = 0;
+        while(j < nums.length){
+            if(!set.contains(nums[j])){
+                sum += nums[j];
+                set.add(nums[j]);
+                j++;
+            }else{
+                while(i < j && nums[i] != nums[j]){
+                    sum -= nums[i];
+                    set.remove(nums[i]);
+                    i++;
                 }
+                i++;
+                j++;
+                
             }
-            if(count == k){
-                sum = Math.max(sum,cur);
+            if(set.size() == k){
+                maxi = Math.max(maxi,sum);
+                sum -= nums[i];
+                set.remove(nums[i]);
+                i++;
             }
+            
         }
-        return sum;
+        return maxi;
     }
 }
