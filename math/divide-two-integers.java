6bf10d151155cf460c79b5e0d0d2fce71c1269a4
Lucky Yadav <luckyyadav1586@gmail.com>
2026-08-24 22:22:21 +0530

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

--- math/divide-two-integers.java
+++ math/divide-two-integers.java
@@ -2,20 +2,23 @@ class Solution {
     public int divide(int dividend, int divisor) {
         if (dividend == Integer.MIN_VALUE && divisor == -1)
             return Integer.MAX_VALUE;
-        int sign = 1;
+        boolean flag = false;
         if(dividend < 0){
-            dividend *= -1;
-            sign *= -1;
+            dividend = Math.abs(dividend);
+            flag = !flag;
         }
         if(divisor < 0){
-            divisor *= -1;
-            sign  *= -1;
+            divisor = Math.abs(divisor);
+            flag = !flag;
         }
             int count = 0;
         while(dividend >= divisor){
             count++;
             dividend -= divisor;
         }
-        return sign * count;
+        if(flag){
+            return 0 - count;
+        }
+        return count;
     }
 }
