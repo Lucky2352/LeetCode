a5409b17ac2f003e874b2c34363137968f6d0c08
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-01 13:42:19 +0530

    LC-4sum-ii [Runtime: N/A; Memory: N/A]

--- hash-table/4sum-ii.java
+++ hash-table/4sum-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int n = nums1.length;
-        int count = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                map.put(nums1[i] + nums2[j],map.getOrDefault(nums1[i] + nums2[j],0)+1);
+        Map<Integer, Integer> abCount = new HashMap<>();
+
+        for(int a : nums1) {
+            for(int b : nums2) {
+                int sum = a + b;
+                abCount.put(sum, abCount.getOrDefault(sum, 0) + 1);
             }
         }
 
-        for(int k = 0;k<n;k++){
-                    for(int l = 0;l<n;l++){
-                        if(map.containsKey((-1 * (nums3[k] + nums4[l])))){
-                            count += map.get((-1 * (nums3[k] + nums4[l])));
-                        }
-                    }
-                }
+        int count = 0;
+        for(int c : nums3) {
+            for(int d : nums4) {
+                int sum = c + d;
+                count += abCount.getOrDefault(-sum, 0);
+            }
+        }
         return count;
     }
 }
