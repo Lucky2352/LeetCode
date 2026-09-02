9a1d8c435408d72101dc1cf0bcc56e6f1848f2cd
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-02 12:57:13 +0530

    LC-subsets-ii [Runtime: N/A; Memory: N/A]

--- array/subsets-ii.java
+++ array/subsets-ii.java
@@ -1,19 +1,19 @@
 class Solution {
-    public static void tp(int i,int n,int[] nums,List<Integer> list ,Set<List<Integer>> st){
+    public static void recursion(int i,int n,int[] nums,List<Integer> list ,Set<List<Integer>> st){
         if(i == n){
             st.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
-        tp(i +1,n,nums,list,st);
+        recursion(i +1,n,nums,list,st);
         list.remove(list.size() - 1);
-        tp(i +1,n,nums,list,st);
+        recursion(i +1,n,nums,list,st);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-         Arrays.sort(nums);
+        Arrays.sort(nums);
         Set<List<Integer>> st = new HashSet<>();
         List<Integer> list = new ArrayList<>();
-        tp(0,nums.length,nums,list,st);
+        recursion(0,nums.length,nums,list,st);
         return new ArrayList(st);
     }
 }
