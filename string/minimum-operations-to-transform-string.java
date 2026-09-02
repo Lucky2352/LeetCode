0078876a7ea4465b3786ed76775a35a794638bbf
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-02 21:51:42 +0530

    LC-minimum-operations-to-transform-string [Runtime: N/A; Memory: N/A]

--- string/minimum-operations-to-transform-string.java
+++ string/minimum-operations-to-transform-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public int minOperations(String s) {
         char check = 'z';
-        for(char c: s.toCharArray()){
-            if(c != 'a'){
-                check = (char)Math.min(c,check);
+        for (char c : s.toCharArray()) {
+            if (c != 'a') {
+                check = (char) Math.min(c, check);
             }
         }
-        if(check == 'z')return 0;
+        if (check == 'z') return 0;
         return check - 'a';
     }
 }
