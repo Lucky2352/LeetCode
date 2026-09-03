2a5aa7a8e9b9d5d5a1046da38e5daecad8f0a1ec
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-03 20:06:58 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- array/construct-uniform-parity-array-ii.java
+++ array/construct-uniform-parity-array-ii.java
@@ -1,36 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
         int min = Integer.MAX_VALUE;
+        boolean flag = false;
         for(int num : nums1){
-            min = Math.min(num,min);
+            min = Math.min(min,num);
+            if(num % 2 != 0)flag = true;
         }
-        if(min % 2 == 0){
-            for(int num : nums1){
-                if(num != min){
-                    if(num % 2 == 0){
-                        continue;
-                    }
-                    else if(num - min >= 1 && (num - min) % 2 == 0){
-                        continue;
-                    }else{
-                        return false;
-                    }
-                }
-            }
-        }else{
-            for(int num : nums1){
-                if(num != min){
-                    if(num % 2 != 0){
-                        continue;
-                    }
-                    else if(num - min >= 1 && (num - min) % 2 != 0){
-                        continue;
-                    }else{
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
+        if(min % 2 == 1)return true;
+        
+        return !flag;
     }
 }
