900eeaa205418bc8b2a4488f1708478a7fd19872
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-03 13:44:05 +0530

    LC-vertical-order-traversal-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- sorting/vertical-order-traversal-of-a-binary-tree.java
+++ sorting/vertical-order-traversal-of-a-binary-tree.java
@@ -1,35 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    Map<Integer, List<Integer>> map = new TreeMap<>();
+    Map<Integer, List<int[]>> map = new TreeMap<>();
     public void check(TreeNode root, int row, int col) {
         if (root == null) return;
         if (!map.containsKey(col)) {
             map.put(col, new ArrayList<>());
         }
-        map.get(col).add(root.val);
-        Collections.sort(map.get(col));
+        map.get(col).add(new int[]{row, root.val});
         check(root.left, row + 1, col - 1);
         check(root.right, row + 1, col + 1);
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         check(root, 0, 0);
-        for (List<Integer> list : map.values()) {
-            ans.add(list);
+        for (List<int[]> list : map.values()) {
+            Collections.sort(list, (a, b) -> {
+                if (a[0] != b[0]) {
+                    return Integer.compare(a[0], b[0]);
+                }
+                return Integer.compare(a[1], b[1]);
+            });
+              List<Integer> temp = new ArrayList<>();
+            for (int[] arr : list) {
+                temp.add(arr[1]);
+            }
+            ans.add(temp);
         }
         return ans;
     }
