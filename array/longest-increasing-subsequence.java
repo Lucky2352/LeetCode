7342bfdd7ac999f6f81333a66cb3bcb37c28d7bf
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-04 14:19:08 +0530

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- array/longest-increasing-subsequence.java
+++ array/longest-increasing-subsequence.java
@@ -1,20 +1,26 @@
 class Solution {
-    int len = 0;
-    public void recursion(int nums[], int i, List<Integer> list) {
+    int len = Integer.MIN_VALUE
+    public void recursion(int nums[], int i, List<Integer> list, int[] dp) {
         if (i == nums.length) {
             len = Math.max(len, list.size());
             return;
         }
+        if (dp[i] != -1) {
+            return;
+        }
         if (list.size() == 0 || list.get(list.size() - 1) < nums[i]) {
             list.add(nums[i]);
-            recursion(nums, i + 1, list);
+            recursion(nums, i + 1, list, dp);
             list.remove(list.size() - 1);
         }
-        recursion(nums, i + 1, list);
+        recursion(nums, i + 1, list, dp);
+        dp[i] = len;
     }
 
     public int lengthOfLIS(int[] nums) {
-        recursion(nums, 0, new ArrayList<>());
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        recursion(nums, 0, new ArrayList<>(), dp);
         return len;
     }
 }
