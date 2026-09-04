eab085a68ff86f3808cb87f58062daa0df5a2b93
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-04 09:25:16 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-i.java
+++ array/smallest-stable-index-i.java
@@ -2,25 +2,16 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int[] forward = new int[nums.length];
         int[] backward = new int[nums.length];
-        forward[0] = nums[0];
         int maxi = nums[0];
-        for(int i = 1;i<nums.length;i++){
-            forward[i] = Math.max(maxi,nums[i]);
-            maxi = Math.max(maxi,nums[i]);
-        }
-
-        backward[nums.length - 1] = nums[nums.length - 1];
         int mini = nums[nums.length - 1];
-
-        for(int i = nums.length - 2;i>=0;i--){
-            backward[i] = Math.min(nums[i],mini);
-            mini = Math.min(nums[i],mini);
+        for(int i = 0;i<nums.length;i++){
+            forward[i] = Math.max(maxi,nums[i]);
+            backward[nums.length - 1 - i] = Math.min(nums[nums.length - 1 - i],mini);
+            maxi = Math.max(maxi,nums[i]);
+            mini = Math.min(nums[nums.length - 1 - i],mini);
         }
-
         for(int i = 0;i<nums.length;i++){
             if((forward[i] - backward[i]) <= k){
-                System.out.println(forward[i] - backward[i]);
-                System.out.println(forward[i] +" "+ backward[i]);
                 return i; 
             }
         }
