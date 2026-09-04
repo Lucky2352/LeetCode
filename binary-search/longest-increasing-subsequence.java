8beb7d1b53be7be457af2327d2ea25b3713666ff
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-04 14:20:24 +0530

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- binary-search/longest-increasing-subsequence.java
+++ binary-search/longest-increasing-subsequence.java
@@ -1,26 +1,28 @@
 class Solution {
-    int len = Integer.MIN_VALUE
-    public void recursion(int nums[], int i, List<Integer> list, int[] dp) {
+    int len = Integer.MIN_VALUE;
+    public void recursion(int nums[], int i, List<Integer> list, int[][] dp, int prev) {
         if (i == nums.length) {
             len = Math.max(len, list.size());
             return;
         }
-        if (dp[i] != -1) {
+        if (dp[i][prev + 1] != -1) {
             return;
         }
-        if (list.size() == 0 || list.get(list.size() - 1) < nums[i]) {
+        if (prev == -1 || nums[prev] < nums[i]) {
             list.add(nums[i]);
-            recursion(nums, i + 1, list, dp);
+            recursion(nums, i + 1, list, dp, i);
             list.remove(list.size() - 1);
         }
-        recursion(nums, i + 1, list, dp);
-        dp[i] = len;
+        recursion(nums, i + 1, list, dp, prev);
+        dp[i][prev + 1] = len;
     }
-
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        recursion(nums, 0, new ArrayList<>(), dp);
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        recursion(nums, 0, new ArrayList<>(), dp, -1);
         return len;
     }
 }
