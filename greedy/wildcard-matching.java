e7f90d167d6e9577db42902ea69fd0eebf02d80e
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-04 19:04:55 +0530

    LC-wildcard-matching [Runtime: N/A; Memory: N/A]

--- greedy/wildcard-matching.java
+++ greedy/wildcard-matching.java
@@ -1,5 +1,7 @@
 class Solution {
-    public static boolean recursion(int i, int j, String s, String p) {
+    boolean[][] dp;
+    boolean[][] visited;
+    public boolean recursion(int i, int j, String s, String p) {
         if (i == s.length() && j == p.length()) {
             return true;
         }
@@ -15,20 +17,24 @@ class Solution {
             }
             return true;
         }
+        if (visited[i][j]) {
+            return dp[i][j];
+        }
+        visited[i][j] = true;
         if (p.charAt(j) == '?') {
-            return recursion(i + 1, j + 1, s, p);
+            return dp[i][j] = recursion(i + 1, j + 1, s, p);
         }
         if (p.charAt(j) == '*') {
-            return recursion(i, j + 1, s, p) ||
-                   recursion(i + 1, j, s, p);
+            return dp[i][j] = recursion(i, j + 1, s, p) || recursion(i + 1, j, s, p);
         }
         if (p.charAt(j) == s.charAt(i)) {
-            return recursion(i + 1, j + 1, s, p);
+            return dp[i][j] = recursion(i + 1, j + 1, s, p);
         }
-        return false;
+        return dp[i][j] = false;
     }
-
     public boolean isMatch(String s, String p) {
+        dp = new boolean[s.length()][p.length()];
+        visited = new boolean[s.length()][p.length()];
         return recursion(0, 0, s, p);
     }
 }
