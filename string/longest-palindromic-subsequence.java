ff0d3f327169d6f1c7d286533d714c6420ccc410
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-04 14:45:26 +0530

    LC-longest-palindromic-subsequence [Runtime: N/A; Memory: N/A]

--- string/longest-palindromic-subsequence.java
+++ string/longest-palindromic-subsequence.java
@@ -1,24 +1,31 @@
 class Solution {
     int maxi = 0;
-    public void recursion(int i,int j,String s,int count){
-        if(i > j){
-            maxi = Math.max(count,maxi);
+    public void recursion(int i, int j, String s, int count, int[][] dp) {
+        if (i > j) {
+            maxi = Math.max(count, maxi);
             return;
         }
-        if(i == j){
-            maxi = Math.max(maxi,count+1);
+        if (i == j) {
+            maxi = Math.max(maxi, count + 1);
             return;
         }
-        if(s.charAt(i) == s.charAt(j)){
-            recursion(i+1,j-1,s,count + 2);
+        if (dp[i][j] != -1 && dp[i][j] >= count) {
+            return;
         }
-        recursion(i+1,j,s,count);
-        recursion(i,j-1,s,count);
-        
-
+        dp[i][j] = count;
+        if (s.charAt(i) == s.charAt(j)) {
+            recursion(i + 1, j - 1, s, count + 2, dp);
+        }
+        recursion(i + 1, j, s, count, dp);
+        recursion(i, j - 1, s, count, dp);
     }
     public int longestPalindromeSubseq(String s) {
-        recursion(0,s.length() - 1,s,0);
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        recursion(0, n - 1, s, 0, dp);
         return maxi;
     }
 }
