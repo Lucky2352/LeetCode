3fffa0267d112a0811307eb6a56dcd1ea72f7fe0
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-06 23:12:06 +0530

    LC-same-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/same-tree.java
+++ binary-tree/same-tree.java
@@ -14,10 +14,23 @@
  * }
  */
 class Solution {
+    public static void trav(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+        list.add(root.val);
+        trav(root.left, list);
+        trav(root.right, list);
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+    trav(p,list1);
+    trav(q,list2);
+    if(list1.size() != list2.size())return false;
+    for(int i = 0;i<list1.size();i++){
+        if(list1.get(i) != list2.get(i))return false;
+    }
+    return
     }
 }
