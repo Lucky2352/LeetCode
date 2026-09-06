3b218d8813c0c140e93767005993ec530be2559c
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-06 21:51:22 +0530

    LC-binary-tree-zigzag-level-order-traversal [Runtime: N/A; Memory: N/A]

--- tree/binary-tree-zigzag-level-order-traversal.java
+++ tree/binary-tree-zigzag-level-order-traversal.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();\
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
         boolean flag = true;
         q.offer(root);
@@ -32,9 +33,8 @@ class Solution {
                     q.offer(temp.right);
                 }
             }
-            if(flag){
+            if(!flag){
                 Collections.reverse(list);
-                flag = false;
             }
                 ans.add(new ArrayList<>(list));
                 flag = !flag;
