c1795daf19f7cb3e4fe029edcd16bd4bfdfde0c7
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-06 23:10:54 +0530

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

--- depth-first-search/binary-tree-maximum-path-sum.java
+++ depth-first-search/binary-tree-maximum-path-sum.java
@@ -15,18 +15,15 @@
  */
 class Solution {
     int maxi = Integer.MIN_VALUE;
-    public int check(TreeNode root) {
-        if (root == null)
-            return 0;
-        int left = check(root.left);
-        int right = check(root.right);
-        left = Math.max(0, left);
-        right = Math.max(0, right);
-        maxi = Math.max(maxi, root.val + left + right);
-        return root.val + Math.max(left, right);
+    public int dfs(TreeNode root){
+        if(root == null)return 0;
+        int left = Math.max(0,dfs(root.left));
+        int right = Math.max(0,dfs(root.right));
+        maxi = Math.max(maxi,left + right + root.val);
+        return root.val + Math.max(left,right);
     }
     public int maxPathSum(TreeNode root) {
-        check(root);
+         dfs(root);
         return maxi;
     }
 }
