5e84c171fcffc486322a2734239cdbb12c50e8da
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-07 22:47:53 +0530

    LC-distinct-subsequences-ii [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences-ii.java
+++ string/distinct-subsequences-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     int MOD = 1000000007;
-    HashSet<String> set = new HashSet<>();
-    public void recursion(int i, char[] chars, String p) {
-        if (i == chars.length) {
-            if (p.length() != 0) {
-                set.add(p);
-            }
-            return;
-        }
-        recursion(i + 1, chars, p + chars[i]);
-        recursion(i + 1, chars, p);
-    }
-
     public int distinctSubseqII(String s) {
-        char[] chars = s.toCharArray();
-        recursion(0, chars, "");
-        return set.size() % MOD;
+        int[] freq = new int[26];
+        Arrays.fill(freq, -1);
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        for (int i = 1;i <= s.length();i++) {
+            int ch = s.charAt(i - 1) - 'a';
+            dp[i] = (2 * dp[i - 1]) % MOD;
+            if (freq[ch] != -1) {
+                dp[i] = (dp[i] - dp[freq[ch]] + MOD) % MOD;
+            }
+            freq[ch] = i;
+        }
+        return (dp[n] - 1 + MOD) % MOD;
     }
 }
