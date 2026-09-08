6cdd10adb7637a40c2edd97ee3f7d4476d631b8f
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-08 11:01:00 +0530

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- depth-first-search/lowest-common-ancestor-of-a-binary-tree.java
+++ depth-first-search/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,20 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public TreeNode dfs(TreeNode root, TreeNode t1,TreeNode t2) {
-        if (root == null) return null;
-        if (root == t1){
-            return root;
-        }
-        if (root == t2){
-            return root;
-        }
-        TreeNode left = dfs(root.left, t1, t2);
-        TreeNode right = dfs(root.right, t1, t2);
+    public static TreeNode dfs(TreeNode root, TreeNode t1, TreeNode t2){
+        if(root == null)return root;
+        if(root == t1)return root;
+        if(root == t2)return root;
+        TreeNode left = dfs(root.left,t1,t2);
+        TreeNode right = dfs(root.right,t1,t2);
         if(left != null && right != null)return root;
-        if(left != null){
-            return left;
-        }else{
+        if(left != null)return left;
+        else{
             return right;
         }
+
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         return dfs(root,p,q);
