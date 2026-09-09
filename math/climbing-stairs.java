d18a8036a7e971069faba3f9dada9a6453a7cb58
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-09 08:17:24 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.java
+++ math/climbing-stairs.java
@@ -1,23 +1,11 @@
 class Solution {
-    public int recursion(int i,int n,int[] dp){
-        if(i == n){
-            return 1;
-        }
-        int total = 0;
-        if(dp[i] != 0)return dp[i];
-        if(i + 1 <= n){
-        total += recursion(i+1,n,dp);
-        dp[i] = total;
-        }
-        if(i + 2 <= n){
-            total += recursion(i+2,n,dp);
-            dp[i] = total;
-        }
-        return total;
-
-    }
     public int climbStairs(int n) {
         int dp[] = new int[n + 1];
-        return recursion(0,n,dp);
+        dp[n] = 1;
+        dp[n - 1] = 1;
+        for(int i = n -2;i>= 0;i--){
+            dp[i] = dp[i +1] + dp[i + 2];
+        }
+        return dp[0];
     }
 }
