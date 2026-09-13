270b7348cafb11d5f360f5de44f50ba094247757
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-13 23:50:03 +0530

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- matrix/search-a-2d-matrix.java
+++ matrix/search-a-2d-matrix.java
@@ -8,10 +8,10 @@ class Solution {
             int mid = low + (high - low)/2;
             if(matrix[mid/m][mid%m] == target){
                 return true;
-            }else if(matrix[mid/m][mid%n] < target){
-                low = mid + 1;
-            }else{
+            }else if(matrix[mid/m][mid%m] < target){
                 high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
         return false;
