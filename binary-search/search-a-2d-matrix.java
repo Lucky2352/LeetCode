4dcd0e5961000c33b9dcc750a1a76800e9bc79e2
Lucky Yadav <luckyyadav1586@gmail.com>
2026-09-13 23:48:26 +0530

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- binary-search/search-a-2d-matrix.java
+++ binary-search/search-a-2d-matrix.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int low = 0;
+        int n = matrix.length;
         int m = matrix[0].length;
-        int high = (matrix.length * m) - 1;
-        while(low <= high){
+        int high = (n * m) - 1;
+        while(low < high){
             int mid = low + (high - low)/2;
-            if(matrix[mid / m][mid % m]==target)return true;
-            else if(matrix[mid / m][mid % m] > target){
-                high = mid - 1;
-            }else{
+            if(matrix[mid/m][mid%m] == target){
+                return true;
+            }else if(matrix[mid/m][mid%n] < target){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
         return false;
